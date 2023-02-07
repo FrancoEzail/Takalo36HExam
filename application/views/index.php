b28fbea50e9a5a3ac6cd2523263710d5a607ca7d
FrancoEzail <124678934+FrancoEzail@users.noreply.github.com>
2023-02-07 19:50:24 +0100

    assets

--- application/views/index.php
+++
@@ -1,9 +0,0 @@
-  <!--header------------>
-    <?php $this->load->view("page/header");?>
-  <!--header-------------->
-  <!-- Page Content -->
-    <?php $this->load->view($content);?>
-  <!-- /.container -->
-  <!-- Footer -->
-    <?php $this->load->view("page/footer");?>
-  <!-----Footer->
