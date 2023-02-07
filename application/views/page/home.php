eeff36cffc079ea7a4ee5e685731d0ba7c621e71
Mitia <mitiaandri5@gmail.com>
2023-02-07 18:31:28 +0100

    homepage

---
+++ application/views/page/home.php
@@ -0,0 +1,56 @@
+<section style="margin: 0px;margin-top: 24px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-5 col-xl-4 offset-xl-0">
+                    <div>
+                        <h5 class="text-center" style="color: rgb(7,43,69);">
+                            <strong>Waiting</strong>
+                        </h5>
+                        <div style="height: 122px;background: #f1f7ff;border-radius: 25;box-shadow: 9px 6px 20px 0px rgba(44,44,44,0.23);margin-top: 32px;padding-left: 10%;padding-top: 2%;"><img class="float-end" style="margin-right: 4%;">
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Nom Produit</p>
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Description</p>
+                            <p class="text-center float-end" style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Prix</p>
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Etat</p>
+                        </div>
+                        <div style="height: 122px;background: #f1f7ff;border-radius: 25;box-shadow: 9px 6px 20px 0px rgba(44,44,44,0.23);margin-top: 32px;padding-left: 10%;padding-top: 2%;"><img class="float-end" style="margin-right: 4%;">
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Nom Produit</p>
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Description</p>
+                            <p class="text-center float-end" style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Prix</p>
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Etat</p>
+                        </div>
+                        <div style="height: 122px;background: #f1f7ff;border-radius: 25;box-shadow: 9px 6px 20px 0px rgba(44,44,44,0.23);margin-top: 32px;padding-left: 10%;padding-top: 2%;"><img class="float-end" style="margin-right: 4%;">
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Nom Produit</p>
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Description</p>
+                            <p class="text-center float-end" style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Prix</p>
+                            <p style="width: 50%;color: rgb(7,43,69);font-weight: bold;">Etat</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-7 offset-lg-0 offset-xl-1">
+                    <div>
+                        <h5 class="text-center" style="color: rgb(7,43,69);">
+                            <strong>Object Able</strong>
+                        </h5>
+                        <div style="height: 122px;background: #f5f5f5;border-radius: 10;box-shadow: 9px 6px 20px 0px rgba(44,44,44,0.21);margin-top: 32px;padding-left: 10%;padding-top: 2%;"><img class="float-end" style="margin-right: 4%;">
+                            <p style="width: 50%;color: rgb(7,43,69);">Nom Produit</p>
+                            <p style="width: 50%;color: rgb(7,43,69);">Description</p>
+                            <p class="text-center float-end" style="width: 50%;color: rgb(7,43,69);">Prix</p>
+                            <p style="width: 50%;color: rgb(7,43,69);">Etat</p>
+                        </div>
+                        <div style="height: 122px;background: #f5f5f5;border-radius: 10;box-shadow: 9px 6px 20px 0px rgba(44,44,44,0.21);margin-top: 32px;padding-left: 10%;padding-top: 2%;"><img class="float-end" style="margin-right: 4%;">
+                            <p style="width: 50%;color: rgb(7,43,69);">Nom Produit</p>
+                            <p style="width: 50%;color: rgb(7,43,69);">Description</p>
+                            <p class="text-center float-end" style="width: 50%;color: rgb(7,43,69);">Prix</p>
+                            <p style="width: 50%;color: rgb(7,43,69);">Etat</p>
+                        </div>
+                        <div style="height: 122px;background: #f5f5f5;border-radius: 10;box-shadow: 9px 6px 20px 0px rgba(44,44,44,0.21);margin-top: 32px;padding-left: 10%;padding-top: 2%;"><img class="float-end" style="margin-right: 4%;">
+                            <p style="width: 50%;color: rgb(7,43,69);">Nom Produit</p>
+                            <p style="width: 50%;color: rgb(7,43,69);">Description</p>
+                            <p class="text-center float-end" style="width: 50%;color: rgb(7,43,69);">Prix</p>
+                            <p style="width: 50%;color: rgb(7,43,69);">Etat</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
