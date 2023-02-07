052bb8841b0539e4d2fb573d7f822296db8d40d9
FrancoEzail <124678934+FrancoEzail@users.noreply.github.com>
2023-02-07 14:08:03 +0100

    footer

---
+++ application/views/page/footer.php
@@ -0,0 +1,29 @@
+<footer class="footer-clean" style="margin-top: 5%;">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Echange</a></li>
+                        <li><a href="#">Chating</a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>Team</h3>
+                    <ul>
+                        <li><a href="#">Personne1</a></li>
+                        <li><a href="#">Personne 2</a></li>
+                        <li><a href="#">Personne 3</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+                    <p class="copyright">Takalo-Takalo © 2023</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
